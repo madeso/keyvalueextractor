05c2ce83f100afe365b8752b91811cd39d8de338
Gustav <sir.gustav.the.coder@gmail.com>
2018-01-24 09:05:35 +0100

    better printing in main

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ mod kve;
 
 use std::path::Path;
 
+fn print(t: &kve::KeyValueExtractor, path: &str)
+{
+    let extracted = t.extract(Path::new(path));
+    println!("{}", path);
+    println!("{:?}", extracted);
+    println!("");
+}
+
 fn main() {
     match kve::KeyValueExtractor::new("%album%/%artist%-%title%") {
         Err(err) => println!("Failed to parse: {:?}", err),
         Ok(t) => {
             println!("//////////////////////////////////");
 
-            println!("{:?}", t.extract(Path::new("songs/crap/Cannibal/Ke$ha-Crazy Beautiful Life.mp3")));
-            println!("{:?}", t.extract(Path::new("songs/One Of The Boys/Katy Perry-I Kissed A Girl.mp3")));
-            println!("{:?}", t.extract(Path::new("All I Ever Wanted/Kelly Clarkson-Long Shot.mp3")));
-            println!("{:?}", t.extract(Path::new("music.mp3")));
-            println!("{:?}", t.extract(Path::new("the las - There she goes again.mp3")));
+            print(&t, "songs/crap/Cannibal/Ke$ha-Crazy Beautiful Life.mp3");
+            print(&t, "songs/One Of The Boys/Katy Perry-I Kissed A Girl.mp3");
+            print(&t, "All I Ever Wanted/Kelly Clarkson-Long Shot.mp3");
+            print(&t, "music.mp3");
+            print(&t, "the las - There she goes again.mp3");
         }
     }
 }
